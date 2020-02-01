b33a72814d6ceb7c3ee4b4d6780b40444f6684f0
Silversunset01 <github@silversunset.net>
2020-02-01 13:04:53 -0500

    Create actions.js

---
+++ actions.js
@@ -0,0 +1,744 @@
+[
+   {
+      "Category":"Messaging",
+      "Action":"Add Embed Field",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Add Item to List",
+      "Description":"Adds an item to a previously created list.",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Add Member Role",
+      "Description":"Adds a role to a specified member",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Add Reaction",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Apply Image Effect",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Ban Member",
+      "Description":"Bans a member from the server",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Call Command/Event",
+      "Description":"Run another command / event from the current command / event",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/callcommandevent.PNG)"
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check if Member Has Role",
+      "Description":"Check if the specified user has a role, often used with `Find Role`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check is Bot in Voice Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Check Member Data",
+      "Description":"Compare member data from `data/players.json` to a specified value",
+      "IMG":""
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check Member Permissions",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check Parameters",
+      "Description":"Verify user input by checking the number/type of parameters entered on a command",
+      "IMG":""
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Check Server Data",
+      "Description":"Compare server data from `data/servers.json` to a specified value",
+      "IMG":""
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check Variable",
+      "Description":"Check the value of a variable, things like 'variable includes' or 'variable equals'. In JavaScript this is an `if... then...`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Conditions",
+      "Action":"Check Variable Type",
+      "Description":"Check the type of a variable, things like 'variable is number' or 'variable is channel'. In JavaScript this is an `if... then...`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Clear Queue",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Control Audio",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Control Member Data",
+      "Description":"Saves member data to the `data/players.json` file",
+      "IMG":""
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Control Server Data",
+      "Description":"Saves server data to the `data/servers.json` file",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Control Variable",
+      "Description":"Sets the value of a variable",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/controlvariable.PNG)"
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Convet List to Text",
+      "Description":"Transforms a list from JSON format `['item','item','item']` to something more friendly like <br/>`- item`<br/>`- item`<br/>`- item`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Create Embed Message",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Emoji Control",
+      "Action":"Create Emoji",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Create Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Create Image from Avatar",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Create Invite",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Create List",
+      "Description":"Creates a list, in JavaScript this is referred to as an Array",
+      "IMG":""
+   
+},
+   {
+      "Category":"Role Control",
+      "Action":"Create Role",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Create Text Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Create Voice Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Delete Bulk Messages",
+      "Description":"Deletes multiple messages. Due to Discords API limits you may only delete 2-100 messages at a time and they must be < 14 days old",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Delete Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Emoji Control",
+      "Action":"Delete Emoji",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Delete Message",
+      "Description":"Delete a single message",
+      "IMG":""
+   
+},
+   {
+      "Category":"Role Control",
+      "Action":"Delete Role",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Draw Image on Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Draw Text on Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Edit Message",
+      "Description":"Edit the content of a message",
+      "IMG":""
+   
+},
+   {
+      "Category":"Role Control",
+      "Action":"Edit Role",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"End Action Sequence",
+      "Description":"Stops all actions in the current command / event (often used in conjunction with `Check Variable`",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/endactionsequence.PNG)"
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Find Channel",
+      "Description":"Get a channel object for a text channel",
+      "IMG":""
+   
+},
+   {
+      "Category":"Emoji Control",
+      "Action":"Find Custom Emoji",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Find Member",
+      "Description":"Obtains the member object",
+      "IMG":""
+   
+},
+   {
+      "Category":"Role Control",
+      "Action":"Find Role",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Find Server",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Find Voice Channel",
+      "Description":"Get a channel object for a voice channel",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Generate Random Number",
+      "Description":"Chooses a random number within a specified range",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/generaterandomnumber.PNG)"
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Get Bot as Member",
+      "Description":"Get the bot member object",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Get Item from List",
+      "Description":"Retrieve an item from a previously created list",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Get List Length",
+      "Description":"Count the number of items in a previously created list",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Join Voice Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Kick Member",
+      "Description":"Kicks a member from the server",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Leave Voice Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Loop Through All Servers",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Loop Through List",
+      "Description":"Cycle through a previously created list and perform actions for each item, requires an event as well that contains all actions to perform for each object. See [this tutorial](https://silversunset.net/dbm/tutorials#miscellaneous-tutorials-lists-loops) for more info",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Pin Message",
+      "Description":"Pin a message to a channel",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Play File",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Play URL",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Play YouTube Video",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Remove Item from List",
+      "Description":"Removes an item from a previously created list",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Remove Member Role",
+      "Description":"Removes a role from a specificed member",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Resize Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Rotate Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Run Script",
+      "Description":"Run custom JavaScript code directly within DBM",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/runscript.PNG)"
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Save Variable",
+      "Description":"Saves a variable to a data file, so the information will be retained when the bot restarts",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Send Embed Message",
+      "Description":"Sends an embed message. Requires `Create Embed Message` before being used",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Send File",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Image Editing",
+      "Action":"Send Image",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Send Message",
+      "Description":"Sends a text message to the specified channel",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Send TTS Message",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Set Audio Volume",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot AFK Status",
+      "Description":"Changes the bots status between AFK/Not AFK",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot Avatar",
+      "Description":"Changes the bots avatar image",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot Game",
+      "Description":"Changes the bots game to `Playing [your text here]`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot Nickname",
+      "Description":"Changes the bots nickname on the server",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot Status",
+      "Description":"Changes the bots status between Online/Idle/Invisible/DND",
+      "IMG":""
+   
+},
+   {
+      "Category":"Bot Client Control",
+      "Action":"Set Bot Username",
+      "Description":"Changes the bots username",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Set Channel Permissions",
+      "Description":"Set the permissions of a specified channel",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Set Embed Description",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Set Member Channel Perms",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Set Member Deafen",
+      "Description":"**Audio** deafens a user in a voice chat",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Set Member Mute",
+      "Description":"**Audio** mutes a member in a voice chat",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Set Member Nickname",
+      "Description":"Changes the users nickname on the server",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Set Member Permissions",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Set Member Voice Channel",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Set Role Channel Perms",
+      "Description":"Set the permissions of a specified role",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Set Server Icon",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Set Server Name",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Set Server Region",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Set Server Splash Screen",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Set Server Verification",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Audio Control",
+      "Action":"Skip Queue",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Role Control",
+      "Action":"Stor Role Info",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Store Channel Info",
+      "Description":"Store information about a text channel, i.e. Name, Topic, Last Message, Position, etc",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Store Command Params",
+      "Description":"Stores parameters for a command. See [this tutorial](https://silversunset.net/dbm/tutorials#data-store-command-parameters) for more info",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/storecommandparams.PNG)"
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Store Member Data",
+      "Description":"Reads member data from the `data/players.json` file",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Store Member Info",
+      "Description":"Stores information about the member, things like username, nickname, avatar URL, game, color, etc",
+      "IMG":""
+   
+},
+   {
+      "Category":"Messaging",
+      "Action":"Store Message Info",
+      "Description":"Stores information about a specified message. **DO NOT use this if you are trying to store parameters** -> instead you should use `Store Command Parameters`",
+      "IMG":""
+   
+},
+   {
+      "Category":"Deprecated",
+      "Action":"Store Server Data",
+      "Description":"Reads server data from the `data/servers.json` file",
+      "IMG":""
+   
+},
+   {
+      "Category":"Server Control",
+      "Action":"Store Server Info",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Store Time Info",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Channel Control",
+      "Action":"Store Voice Channel Info",
+      "Description":"Store information about a voice channel, i.e. Name, Topic, Last Message, Position, etc",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Transfer Variable",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Lists and Loops",
+      "Action":"Transform List",
+      "Description":"",
+      "IMG":""
+   
+},
+   {
+      "Category":"Member Control",
+      "Action":"Unban Member",
+      "Description":"Unbans a member from the server",
+      "IMG":""
+   
+},
+   {
+      "Category":"Other Stuff",
+      "Action":"Wait",
+      "Description":"",
+      "IMG":"[link](https://raw.githubusercontent.com/Silversunset01/dbm/master/screenshots/actions/wait.PNG)"
+   
+}
+]
